2cfd4a3211b9b709b34b3cc34ce03ba557f4342b
sczyh30 <sczyh16@gmail.com>
2016-08-03 19:56:25 +0800

    Update gh-pages

--- kue-core/src/main/java/io/vertx/blueprint/kue/queue/package-info.java
+++
@@ -1,4 +0,0 @@
-@ModuleGen(groupPackage = "io.vertx.blueprint.kue.queue", name = "vertx-kue-queue-core-module")
-package io.vertx.blueprint.kue.queue;
-
-import io.vertx.codegen.annotations.ModuleGen;
